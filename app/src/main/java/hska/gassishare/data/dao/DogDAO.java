8c461bf5b7655de3a617a11d31a597ef68167407
Hanna Grether <hanna.grether@googlemail.com>
2023-05-27 16:59:12 +0200

    DAOs hinzugefuegt

--- app/src/main/java/hska/gassishare/data/dao/DogDAO.java
+++ app/src/main/java/hska/gassishare/data/dao/DogDAO.java
@@ -1,4 +1,25 @@
 package hska.gassishare.data.dao;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
+
+import hska.gassishare.data.entity.Dog;
+
 public interface DogDAO {
+    // LiveData is a data holder class that can be observed within a given lifecycle.
+    // Always holds/caches latest version of data. Notifies its active observers when the
+    // data has changed. Since we are getting all the contents of the database,
+    // we are notified whenever any of the database contents have changed.
+    @Query("SELECT * FROM dog_table ORDER BY id ASC")
+    LiveData<List<Dog>> getAlphabetizedWords();
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Dog dog);
+
+    @Query("DELETE FROM dog_table")
+    void deleteAll();
 }
--- app/src/main/java/hska/gassishare/data/dao/UserDAO.java
+++ app/src/main/java/hska/gassishare/data/dao/UserDAO.java
@@ -1,4 +1,25 @@
 package hska.gassishare.data.dao;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
+
+import hska.gassishare.data.entity.User;
+
 public interface UserDAO {
+    // LiveData is a data holder class that can be observed within a given lifecycle.
+    // Always holds/caches latest version of data. Notifies its active observers when the
+    // data has changed. Since we are getting all the contents of the database,
+    // we are notified whenever any of the database contents have changed.
+    @Query("SELECT * FROM user_table ORDER BY username ASC")
+    LiveData<List<User>> getAlphabetizedWords();
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(User user);
+
+    @Query("DELETE FROM user_table")
+    void deleteAll();
 }
