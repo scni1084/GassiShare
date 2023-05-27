59dc35aa9a8e06fbdd8528a0a7008af7b39a8307
Hanna Grether <hanna.grether@googlemail.com>
2023-05-27 15:32:48 +0200

    Entities fuer dog und user hinzugefuegt

--- app/src/main/java/hska/gassishare/data/entity/Dog.java
+++ app/src/main/java/hska/gassishare/data/entity/Dog.java
@@ -1,4 +1,75 @@
 package hska.gassishare.data.entity;
 
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "dog_table")
 public class Dog {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    //TODO: FK-Beziehung herstellen so korrekt?
+    @NonNull
+    private int user_id;
+
+    @NonNull
+    private String name;
+
+    private Integer alter;
+
+    private String geschlecht;
+
+    private String rasse;
+
+    private Boolean kastriert;
+
+    private String beschreibung;
+
+    public Dog(int id, int user_id, @NonNull String name, Integer alter, String geschlecht, String rasse, Boolean kastriert, String beschreibung) {
+        this.id = id;
+        this.user_id = user_id;
+        this.name = name;
+        this.alter = alter;
+        this.geschlecht = geschlecht;
+        this.rasse = rasse;
+        this.kastriert = kastriert;
+        this.beschreibung = beschreibung;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public int getUser_id() {
+        return this.user_id;
+    }
+
+    @NonNull
+    public String getName() {
+        return this.name;
+    }
+
+    public Integer getAlter() {
+        return this.alter;
+    }
+
+    public String getGeschlecht() {
+        return this.geschlecht;
+    }
+
+    public String getRasse() {
+        return this.rasse;
+    }
+
+    public Boolean getKastriert() {
+        return this.kastriert;
+    }
+
+    public String getBeschreibung() {
+        return this.beschreibung;
+    }
 }
+
--- app/src/main/java/hska/gassishare/data/entity/User.java
+++ app/src/main/java/hska/gassishare/data/entity/User.java
@@ -1,4 +1,101 @@
 package hska.gassishare.data.entity;
 
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "user_table")
 public class User {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @NonNull
+    private String username;
+    //..other fields, getters, setters
+
+    @NonNull
+    private String nachname;
+
+    @NonNull
+    private String vorname;
+
+    @NonNull
+    private String passwort;
+
+    @NonNull
+    private String email;
+
+    private String beschreibung;
+
+    @NonNull
+    private Integer plz;
+
+    @NonNull
+    private String strasse;
+
+    @NonNull
+    private String ort;
+
+
+    public User(int id, @NonNull String username, @NonNull String nachname, @NonNull String vorname, @NonNull String passwort, @NonNull String email, String beschreibung, @NonNull Integer plz, @NonNull String strasse, @NonNull String ort) {
+        this.id = id;
+        this.username = username;
+        this.nachname = nachname;
+        this.vorname = vorname;
+        this.passwort = passwort;
+        this.email = email;
+        this.beschreibung = beschreibung;
+        this.plz = plz;
+        this.strasse = strasse;
+        this.ort = ort;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    @NonNull
+    public String getUsername() {
+        return this.username;
+    }
+
+    @NonNull
+    public String getNachname() {
+        return this.nachname;
+    }
+
+    @NonNull
+    public String getVorname() {
+        return this.vorname;
+    }
+
+    @NonNull
+    public String getPasswort() {
+        return this.passwort;
+    }
+
+    @NonNull
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getBeschreibung() {
+        return this.beschreibung;
+    }
+
+    @NonNull
+    public Integer getPlz() {
+        return this.plz;
+    }
+
+    @NonNull
+    public String getStrasse() {
+        return this.strasse;
+    }
+
+    @NonNull
+    public String getOrt() {
+        return this.ort;
+    }
 }
