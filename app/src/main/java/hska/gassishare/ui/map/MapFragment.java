ffd5d2217b8f2ceed6715715f19f07515478f04c
Hanna Grether <hanna.grether@googlemail.com>
2023-06-13 16:28:35 +0200

    Karte und Marker eingefuegt

--- app/src/main/java/hska/gassishare/ui/map/MapFragment.java
+++ app/src/main/java/hska/gassishare/ui/map/MapFragment.java
@@ -3,6 +3,7 @@ package hska.gassishare.ui.map;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
@@ -21,6 +22,9 @@ import org.osmdroid.config.Configuration;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
+import org.osmdroid.views.overlay.OverlayItem;
 
 import java.util.ArrayList;
 
@@ -65,6 +69,49 @@ public class MapFragment extends Fragment {
         mapController.setZoom(16);
         GeoPoint startPoint = new GeoPoint(49.014, 8.404);
         mapController.setCenter(startPoint);
+
+        //Standortmarker setzen
+        // Create an ArrayList to hold your items
+        ArrayList<OverlayItem> items = new ArrayList<OverlayItem>();
+        Drawable doggoIcon1 = ContextCompat.getDrawable(getActivity(), R.drawable.doggo_marker1);
+        Drawable doggoIcon2 = ContextCompat.getDrawable(getActivity(), R.drawable.doggo_marker2);
+        Drawable doggoIcon3 = ContextCompat.getDrawable(getActivity(), R.drawable.doggo_marker3);
+
+        // Add an item for each marker you want to display
+        GeoPoint pointDog1 = new GeoPoint(49.006889, 8.40299);
+        GeoPoint pointDog2 = new GeoPoint(49.021435, 8.395378);
+        GeoPoint pointDog3 = new GeoPoint(49.013173, 8.407080);
+        OverlayItem dog1 = new OverlayItem("Bello", "Ein verspielter Bello", pointDog1);
+        OverlayItem dog2 = new OverlayItem("Luna", "Eine fröhliche Luna", pointDog2);
+        OverlayItem dog3 = new OverlayItem("Max", "Ein energiegeladener Max", pointDog3);
+
+        dog1.setMarker(doggoIcon1);
+        dog2.setMarker(doggoIcon2);
+        dog3.setMarker(doggoIcon3);
+        items.add(dog1);
+        items.add(dog2);
+        items.add(dog3);
+
+        // Create a new ItemizedIconOverlay with your items
+        ItemizedOverlayWithFocus<OverlayItem> mOverlay = new ItemizedOverlayWithFocus<OverlayItem>(getActivity(), items,
+                new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
+                    @Override
+                    public boolean onItemSingleTapUp(final int index, final OverlayItem item) {
+                        // Do something when item is tapped (optional)
+                        return true;
+                    }
+                    @Override
+                    public boolean onItemLongPress(final int index, final OverlayItem item) {
+                        // Do something when item is long-pressed (optional)
+                        return true;
+                    }
+                });
+
+        // Set to true if you want the item's info window to appear when it's tapped (optional)
+        mOverlay.setFocusItemsOnTap(true);
+
+        // Add the overlay to the MapView
+        map.getOverlays().add(mOverlay);
     }
 
     @Override
